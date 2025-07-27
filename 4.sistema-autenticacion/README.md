eea6f55200f1547035e31d829770386c3e1dbe14
CarlosBarrientoss <carlosabarrientoss@gmail.com>
2025-07-27 17:02:36 -0500

    Agregando laravel Permission

---
+++ 4.sistema-autenticacion/README.md
@@ -0,0 +1,9 @@
+## Sistema Laravel 12
+
+- **Clase 2:** Agregar Campo Activo.
+- **Clase 2:** Listado, paginación y busqueda de registros.
+- **Clase 2:** Creación de registros.
+- **Clase 2:** Edición de registros.
+- **Clase 2:** Eliminación de registros.
+- **Clase 2:** Activación y desactivación de registros.
+
---
+++ 4.sistema-autenticacion/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('usuarios', UserController::class);
+
+Route::patch('usuarios/{usuario}/toggle', [UserController::class, 'toggleStatus'])->name('usuarios.toggle');
